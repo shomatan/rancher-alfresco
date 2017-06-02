ab4df75289888c3dc45c2de967939a0b60d7c2b4
Shoma Nishitateno <shoma416@gmail.com>
2017-06-02 11:49:59 +0900

    Fix - SSL CA cert problem.

--- assets/setup/install_mysql_connector.sh
+++ assets/setup/install_mysql_connector.sh
@@ -4,7 +4,7 @@ set -e
 CONNECTOR=mysql-connector-java-5.1.38
 
 cd /tmp
-curl -OL http://dev.mysql.com/get/Downloads/Connector-J/${CONNECTOR}.tar.gz
+curl -k -OL http://dev.mysql.com/get/Downloads/Connector-J/${CONNECTOR}.tar.gz
 tar xvzf ${CONNECTOR}.tar.gz ${CONNECTOR}/${CONNECTOR}-bin.jar
 
 mv ${CONNECTOR}/${CONNECTOR}-bin.jar ${ALF_HOME}/tomcat/lib
